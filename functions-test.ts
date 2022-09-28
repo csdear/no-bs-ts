86c6e2aa252a19cb65228907c59afa882bd96561
Stuart Dear <stuart.dear@intermarkgroup.com>
2022-09-28 09:07:24 -0500

    ts functions1

---
+++ functions-test.ts
@@ -0,0 +1,9 @@
+// [functions-tests.ts]
+// for importing and testing our functions out
+// if you export const you  can destructure it.
+import addNumbers, { addStrings } from './functions';
+
+console.log(addNumbers(1, 2));
+console.log(addStrings("a", "b"));
+
+
---
+++ functions.ts
@@ -0,0 +1,35 @@
+//  [functions.ts]
+// Basic paramter typing
+function addNumbers(a:number, b:number)  {
+    return a + b;
+}
+
+// Note in ts we we dont like module.exports...
+    // module.exports = addNumbers;
+//...Rather
+export default addNumbers;
+
+// you can do default parameter values too. eg add str3: string = "foobar"
+export const addStrings = (str1: string, str2: string): string => `${str1} ${str2}`;
+
+// union type
+export const format = (title: string, param : string | number): string => `${title} ${param}`
+
+// void function
+// use CMD K, CMD I, which tells us it is of type  void
+// so we just color void it.
+export const printFormat = (title: string, param : string | number): void => {
+    console.log(format(title,param));
+}
+
+//promise function
+// CMD k CMD I = Promise<string>
+// note if you are getting error  like promise is not defined, go to tsconfig and  update "target": "esnext"
+export const fetchData = (url: string): Promise<string> => Promise.resolve((`Data from ${url}`));
+
+// ts rest parameters
+// any parameter received after the salutation is put into an array
+// of names (rest parameters)
+function introduce(salutation: string, ...names: string[] ): string {
+    return `${salutation} ${names.join(' ')}`
+}
